b5ceea0986d421f3e1b0aa4be731dd15a625c77a
prashanthichiri <prasn25@gmail.com>
2026-05-14 18:30:29 -0500

    My first commit

---
+++ src/main/java/com/example/employee/controller/EmployeeController.java
@@ -0,0 +1,32 @@
+package com.example.employee.controller;
+
+import com.example.employee.model.Employee;
+import com.example.employee.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping
+    public List<Employee> getEmployees() {
+        return employeeService.getEmployees();
+    }
+
+    @PostMapping
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return employeeService.addEmployee(employee);
+    }
+
+    // Hello API
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "Hello from lcal";
+    }
+}
---
+++ src/main/java/com/example/employee/service/EmployeeService.java
@@ -0,0 +1,22 @@
+package com.example.employee.service;
+
+import com.example.employee.model.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+
+    private List<Employee> employeeList = new ArrayList<>();
+
+    public List<Employee> getEmployees() {
+        return employeeList;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employeeList.add(employee);
+        return employee;
+    }
+}
